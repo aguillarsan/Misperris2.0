b62180f169e8d004d5186f1fb13163734d1e445a
viictordanieel <vi.reyesp@alumnos.duoc.cl>
2018-10-25 15:00:03 -0300

    supercambio

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,registro
 
 urlpatterns = [
     path('', index,name='index'),
-     
-    
+    path('registro/',registro,name='reg'), #al colocar en el navegador "registro" ira al metodo de la view llamado registro, y a futuro para llamarlo desde una pagina web le damos un nombre name='reg'  
 
 ]
--- core/views.py
+++ core/views.py
@@ -5,29 +5,28 @@ def index(request):
     
 
 
-##def registro(request):
-  #  cliente= Cliente.objects.alls()
-   ##    rut=request.POST.get("rut","")
-     #  nombres=request.POST.get("nombres","")
-      #  apellidos=request.POST.get("apellidos","")
-      #  telefono=request.POST.get("telefono","")
-        #fechaNaci=request.POST.get("fechaNaci","")
-        #region=request.POST.get("region","")
-       #ciudad=request.POST.get("ciudad","")
-       # tipoViv=request.POST.get("tipoViv","")
-       # correo=request.POST.get("correo","")
-       
-        #cli= Cliente(
-            #nombres=nombres,
-            #apellidos=apellidos,
-            #rut=rut,
-           # telefono=telefono,
-          #  fechaNaci=fechaNaci,
-         #   region=region,
-        #    ciudad=ciudad,
-       #     tipoViv=tipoViv,
-      #    cli.save() 
-   #     return render(request,'core/formulario.html',{'cli':cliente,'grabo':True})
-  ##  else:       
-##        return render(request,'core/formulario.html',{'cli':cliente,'grabo':False})
-###
+def registro(request):
+  cliente = Cliente.objects.all()
+  if request.POST:
+    rut=request.POST.get("txt_rut","")
+    nombres=request.POST.get("txt_nombres","")
+    apellidos=request.POST.get("apellidos","")
+    telefono=request.POST.get("telefono","")
+    fechaNaci=request.POST.get("fechaNaci","")
+    region=request.POST.get("region","")
+    ciudad=request.POST.get("ciudad","")
+    tipoViv=request.POST.get("tipoViv","")
+    correo=request.POST.get("correo","")
+    cli= Cliente(
+        nombres=nombres,
+        apellidos=apellidos,
+        rut=rut,
+        telefono=telefono,
+        fechaNaci=fechaNaci,
+        region=region,
+        ciudad=ciudad,
+        tipoViv=tipoViv)
+    cli.save() 
+    return render(request,'core/formulario.html',{'cli':cliente,'grabo':True})
+  else:       
+    return render(request,'core/formulario.html',{'cli':cliente,'grabo':False})
